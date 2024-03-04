70b01627d9a120439b01a1dff960470f975e461a
Mangesh Bangar <100833758+Bangarm654@users.noreply.github.com>
2024-03-04 19:32:19 +0530

    Add files via upload

---
+++ Program58.java
@@ -0,0 +1,28 @@
+class Program58
+{
+	public static void main(String[] args)
+	{
+		int n=5;
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print((char)(j+96));
+			}
+			System.out.println();
+		}
+	}
+}
+
+/*
+e
+de
+cde
+bcde
+abcde
+5
+45
+345
+2345
+12345
+*/
