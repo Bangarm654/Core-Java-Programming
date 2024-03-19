c97eef4d3627a8a9ff0b24d95d96c907e0f6ef13
Mangesh Bangar <100833758+Bangarm654@users.noreply.github.com>
2024-03-19 22:43:56 +0530

    Add files via upload

---
+++ Program130.java
@@ -0,0 +1,30 @@
+class Program130
+{
+	public static void main(String []args)
+	{
+		int n=5;
+		int count=n;
+		for(int i=n;i>=1;i--)
+		{
+			int c=count;//9
+			for(int j=i;j>1;j--)
+			{
+				System.out.print("\t");
+			}
+			for(int j=i;j<=n;j++)
+			{
+				System.out.print(c+"\t");
+				c-=j+1; //9-=4+1
+			}
+			System.out.println();
+			count+=i-1;
+		}
+	}
+}
+/*
+				5
+			9	4
+		12	8	3
+	14	11	7	2
+15	13	10	6	1
+*/
